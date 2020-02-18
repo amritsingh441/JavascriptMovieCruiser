1c09f03eb557b3a5750f7b833cc7de12913f2d50
Ubuntu <ubuntu@ip-172-31-32-139.ec2.internal>
2020-02-18 13:29:28 +0000

    modified code

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
-var favMovieList;
+var favMovieList = [];
+var movieList = [];
 function getMovies() {
 	fetch("http://localhost:3000/movies").then(result => {
 		if(result.status == 200){
@@ -9,14 +10,13 @@ function getMovies() {
 	}).then(moviesData => {
 		console.log("in getMovies :: "+moviesData);
 		addMoviesDataToDOM(moviesData);
+		movieList = moviesData;
 		return moviesData;
 	}).catch(error => {
 		throw new Error(error);
 	});
 	function addMoviesDataToDOM(moviesData){
 		moviesData.forEach(movie => {
-			//let linkCount = 0;
-			let movieId = movie.id;
 			const baseURL = "https://image.tmdb.org/t/p/original";
 			let posterURL = movie.poster_path;
 			let imgURL = baseURL + posterURL;
@@ -40,9 +40,8 @@ function getMovies() {
 			let link1 = document.createElement("a");
 			link1.setAttribute("class","btn btn-primary");
 			link1.textContent = "Add to Favorites";
-			link1.id=movieId;
 			link1.addEventListener("click",()=>{
-				addFavourite(movie,event);
+				addFavourite(movie.id);
 			});
 			div2.appendChild(img1);
 			div2.appendChild(h5_1);
@@ -54,11 +53,9 @@ function getMovies() {
 		}
 }
 function getFavourites() {
-	//document.getElementById("favouritesList").remove();
-	 fetch("http://localhost:3000/favourites").then(favMovie => {
-	 	if(favMovie.status == 200){
-			//document.getElementById("favouritesList").remove();
-			return Promise.resolve(favMovie.json());
+	 fetch("http://localhost:3000/favourites").then(response => {
+	 	if(response.status == 200){
+			return response.json();
 		}else{
 	 		return Promise.reject("Rejecting promise. Could not resolve favourites.");
 		}
@@ -71,74 +68,70 @@ function getFavourites() {
 		throw new Error(error);
 	});
 	
-	function addFavMoviesDataToDOM(favMoviesData){
-		//document.getElementById("favouritesList").
-		console.log("in addFavMoviesDataToDOM :: "+favMoviesData);
-		favMoviesData.forEach(movie => {
-			//let linkCount = 0;
-			if(favMovieList!=null && favMovieList!=undefined){
-				favMovieList.forEach(movies => {
-					if(movies.id == movie.id){
-						return;
-					}
-				})
-			}
-			let movieId = movie.id;
-			const baseURL = "https://image.tmdb.org/t/p/original";
-			let posterURL = movie.poster_path;
-			let imgURL = baseURL + posterURL;
-			let title = movie.title;
-
-			//creating first div element and setting attributes
-			let div1 = document.createElement("div");
-			div1.setAttribute("class","card");
-			div1.setAttribute("style","width: 18rem");
-
-			let img1 = document.createElement("img");
-			img1.setAttribute("class","card-img-top");
-			img1.src = imgURL;
-
-			let div2 = document.createElement("div");
-			div2.setAttribute("class","card-body");
-
-			let h5_1 = document.createElement("h5");
-			h5_1.textContent = title;
-			div2.appendChild(img1);
-			div2.appendChild(h5_1);
-			div1.appendChild(div2);
-			document.getElementById("favouritesList").appendChild(div1);
-			
-		});
-		}
 }
 
-function addFavourite(movie,event) {
-	console.log(event);
-fetch('http://localhost:3000/favourites', 
-{
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(movie),
-}).then((response) => {
-	if(response.status == 201){
-	//getFavourites();
-	//addFavMoviesDataToDOM(newlyAddedMovie);
-	return response.json();
-	 //return Promise.resolve("Added to fav movie list :: "+response.json());
-	}else{
-		return Promise.reject("Error in adding fav movie");
+function addFavMoviesDataToDOM(favMoviesData){
+	console.log("in addFavMoviesDataToDOM :: "+favMoviesData);
+	return favMoviesData.forEach(movie => {
+		const baseURL = "https://image.tmdb.org/t/p/original";
+		let posterURL = movie.poster_path;
+		let imgURL = baseURL + posterURL;
+		let title = movie.title;
+
+		//creating div element and setting attributes
+		let div1 = document.createElement("div");
+		div1.setAttribute("class","card");
+		div1.setAttribute("style","width: 18rem");
+		//creating img element and setting attributes
+		let img1 = document.createElement("img");
+		img1.setAttribute("class","card-img-top");
+		img1.src = imgURL;
+		//creating div element and setting attributes
+		let div2 = document.createElement("div");
+		div2.setAttribute("class","card-body");
+		//creating header element and setting attributes
+		let h5_1 = document.createElement("h5");
+		h5_1.textContent = title;
+		div2.appendChild(img1);
+		div2.appendChild(h5_1);
+		div1.appendChild(div2);
+		document.getElementById("favouritesList").appendChild(div1);
+	});
 	}
+
+function addFavourite(movieId) {
+let needToAddMovie = movieList.filter(m => {
+	return m.id === movieId;
 })
- .then((newlyAddedMovie) =>{
-	 //addFavMoviesDataToDOM(newlyAddedMovie);
-	 getFavourites()
- 	return addedMovie;
- })
-.catch((error) => {
-  return new Error(error);
-});
+let needToAddMovieExisted = favMovieList.filter(m => {
+	return m.id === movieId;
+})
+if(needToAddMovieExisted.length>0){
+	return new Error("Movie already in Favourite List.");
+}else{
+	fetch('http://localhost:3000/favourites', 
+	{
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json',
+	  },
+	  body: JSON.stringify(needToAddMovie[0]),
+	}).then((response) => {
+		if(response.status == 201 || response.status == 200){
+		 return response.json();
+		}else{
+			return Promise.reject("Error in adding fav movie");
+		}
+	})
+	 .then((newlyAddedMovie) =>{
+		 addFavMoviesDataToDOM(newlyAddedMovie);
+		 favMovieList.push(newlyAddedMovie);
+		 return favMovieList;
+	 })
+	.catch((error) => {
+	  return new Error(error);
+	});
+}
 }
 
 module.exports = {
